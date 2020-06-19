48bf79ad132dda4e1cbaf9e3acf89673a6a825ca
alirezainjast <allalirezamail@gmail.com>
2020-06-19 18:19:40 +0430

    add nested components

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import User from './User';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first react app</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>My first react app</h1>
+        <p>Welcome :)</p>
+        <User />
+      </div>
+    );  
+  }  
 }
 
 export default App;
---
+++ my-first-react-app/src/User.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class User extends Component{
+    render() {
+        return (
+            <div className="user">
+                <div>Name: karim</div>
+                <div>Age: 19</div>
+                <div>Sport: Basketball</div>
+            </div>
+        )
+    }
+}
+
+export default User
