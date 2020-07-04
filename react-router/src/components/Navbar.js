172887c19737a60e4209ac3bb0536c285c6ced02
alirezainjast <allalirezamail@gmail.com>
2020-07-04 17:31:34 +0430

    use NavLinks

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () =>{
     return(
@@ -6,9 +7,9 @@ const Navbar = () =>{
             <div className="nav-wrapper deep-purple accent-4">
             <a href="/" className="brand-logo">Karim</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/shop">Shop</a></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/shop">Shop</NavLink></li>
             </ul>
             </div>
         </nav>
