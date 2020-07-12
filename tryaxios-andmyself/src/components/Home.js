e17273a6fb9fc276d91b88d496842e32b04f388b
alirezainjast <allalirezamail@gmail.com>
2020-07-12 13:28:14 +0430

    add LastPosts.js

--- tryaxios-andmyself/src/components/Home.js
+++ tryaxios-andmyself/src/components/Home.js
@@ -1,43 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import LastPosts from './LastPosts';
 
 class Home extends Component {
-    state = {
-        data:{
-            posts: []
-        }
-        
-    }
-    componentDidMount(){
-        axios.get("https://baconipsum.com/api/?type=meat-and-filler")
-            .then(res =>{
-                this.setState({data: {posts: res.data}})
-            })
-    }
-    render(){
-        const posts = this.state.data.posts;
-        const postsList = posts.map(post =>{
-            return(
-                <div className="container kBoxContainer">
-                <div className="row container">
-                    <div className="col s12 m12">
-                    <div className="card grey darken-4 z-depth-2">
-                        <div className="card-content white-text">
-                        <span className="card-title">
-                        </span>
-                        <p>{post}</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            )
-        })
-        return(
-            <div>
-                {postsList}
 
-            </div>
+    render(){
+        return(
+            <LastPosts />
         )
     }
 }
---
+++ tryaxios-andmyself/src/components/LastPosts.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class LastPosts extends Component {
+    state = {
+        data:{
+            posts: []
+        }
+        
+    }
+    componentDidMount(){
+        axios.get("https://baconipsum.com/api/?type=meat-and-filler")
+            .then(res =>{
+                this.setState({data: {posts: res.data}})
+            })
+    }
+    render(){
+        const posts = this.state.data.posts;
+        const postsList = posts.map(post =>{
+            return(
+                <div className="container kBoxContainer">
+                <div className="row container">
+                    <div className="col s12 m12">
+                    <div className="card grey darken-4 z-depth-2">
+                        <div className="card-content white-text">
+                        <span className="card-title">
+                        </span>
+                        <p>{post}</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            )
+        })
+        return(
+            <div>
+                {postsList}
+
+            </div>
+        )
+    }
+}
+
+export default LastPosts
