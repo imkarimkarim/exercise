0ba85ceb8bd57cbe43be237e0ace41b191ea4017
imkarimkarim <imkarimkarim@pm.me>
2023-09-15 12:05:42 +0330

    some py

--- python/user_input.py
+++ python/user_input.py
@@ -1,3 +1,6 @@
 age = input('how old are you bitch?!: ')
-age_in_days = int(age) * 365
-print('haha you lived around', age_in_days, 'days')
+try:
+    age_in_days = int(age) * 365
+    print('haha you lived around', age_in_days, 'days')
+except ValueError:
+    print('you basterd... only input numbers...')
